cc33c988030705f89ad757e2c18acd2d14422987
Ankit Yadav <96226262+virtualknight8@users.noreply.github.com>
2023-09-18 16:31:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,35 +15,7 @@ db_params = {
 
 @app.route('/')
 def index():
-    # Establish a connection to the database
-    conn = psycopg2.connect(**db_params)
-
-    # Create a cursor object to interact with the database
-    cur = conn.cursor()
-
-    # Execute the SQL query to retrieve inventory items with category and supplier information
-    cur.execute("""SELECT * FROM inventory""")
-
-    # Fetch all rows of the result
-    inventory_items = cur.fetchall()
-
-    # Close the cursor and the connection
-    cur.close()
-    conn.close()
-
-    # Convert the data to a suitable format (e.g., list of dictionaries)
-    inventory_data = []
-    for item in inventory_items:
-        item_data = {
-            'id': item[0],
-            'item': item[1],
-            'price': float(item[2]),
-            'quantity': item[3]
-        }
-        inventory_data.append(item_data)
-
-    # Return the inventory data as JSON response
-    return jsonify(inventory_data)
+   return db_params
 
 
 if __name__ == '__main__':
