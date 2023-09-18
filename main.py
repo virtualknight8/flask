def8635128202ac6995ab7f7d2a8094d67e752e1
Ankit Yadav <96226262+virtualknight8@users.noreply.github.com>
2023-09-18 18:30:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,9 @@
 from flask import Flask, jsonify, request
 import os
-import psycopg2
-from flask_cors import CORS
+
 
 app = Flask(__name__)
-CORS(app)
-
-db_params = {
-    'host': ${{Postgres.PGHOST}},
-    'database': ${{Postgres.PGDATABASE}},
-    'user': ${{Postgres.PGUSER}}',
-    'password': '${{Postgres.PGPASSWORD}}'
-}
-
-@app.route('/')
-def index():
-   return db_params
+print('kuch bhi')
 
 
 if __name__ == '__main__':
